086ef89cb144f38445295c4e27ba397daa756686
edgar <jedgarf5@gmail.com>
2019-08-22 21:12:04 +0800

    Add project files.

---
+++ WindowsApplication2/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked Then
+            cb_food.Enabled = True
+            cb_drink.Enabled = True
+            nd_food.Enabled = True
+            nd_drink.Enabled = True
+        Else
+            cb_food.Enabled = False
+            cb_drink.Enabled = False
+            nd_food.Enabled = False
+            nd_drink.Enabled = False
+        End If
+    End Sub
+
+End Class
